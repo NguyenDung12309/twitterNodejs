ad58918644a2441e385a3671106e6d3a55128f0b
dungnn-picon <dung.nn@picontechnology.com>
2024-02-14 23:57:51 +0700

    feat: sample routing

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const name = 'Dư Thanh Được'
-console.log(name)
+import express from 'express'
+import { userRouter } from './routers/user'
+
+const app = express()
+const port = 3000
+app.post('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/user', userRouter)
+
+app.listen(port, () => {
+  console.log('Example app listening on port 3000!')
+})
