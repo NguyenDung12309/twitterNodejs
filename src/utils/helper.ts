6a86d7d56da4e05e9ba9fd1b1a170b0acd2d522a
dungnn-picon <dung.nn@picontechnology.com>
2024-02-17 09:22:41 +0700

    feat: create error controller

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,7 +1,19 @@
+import { MiddleWare } from './../models/middlewares'
+import { RequestHandler } from 'express'
 import { pick } from 'lodash'
 
 export const objectAssign = (data: object, source: any): void => {
   const filteredData = pick(data, Object.keys(source))
 
   Object.assign(source, filteredData)
+}
+
+export const wrapAsync = (func: RequestHandler): MiddleWare => {
+  return async (req, res, next) => {
+    try {
+      await func(req, res, next)
+    } catch (err) {
+      next(err)
+    }
+  }
 }
